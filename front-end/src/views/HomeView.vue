93c5a4902a41957874bb908974226f20d509f522
Skiroe <r.s.canak@gmail.com>
2022-09-22 16:18:20 +0200

    Changes by Max

--- front-end/src/views/HomeView.vue
+++ front-end/src/views/HomeView.vue
@@ -1,9 +1,43 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import List from "../components/List";
+
+
+
+export default {
+
+  created() {
+    this.accounts = [
+      {
+        id: 1,
+        name: 'Max',
+      },
+      {
+        id: 2,
+        name: 'Sel',
+      },
+      {
+        id: 3,
+        name: 'Kes',
+      },
+    ]
+  },
+  data() {
+    return {
+      accounts: []
+    }
+  }
+}
+
+
+
+
+
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <List :array="accounts"></List>
   </main>
 </template>
