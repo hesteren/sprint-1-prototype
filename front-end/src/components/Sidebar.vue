4981c2f3edc23d7ad37d9d01f0275c7538907e88
Kester van Leeuwen <kester@posonas.com>
2022-09-22 22:41:48 +0200

    trynna fix logo

--- front-end/src/components/Sidebar.vue
+++ front-end/src/components/Sidebar.vue
@@ -45,7 +45,7 @@
 
 <script setup>
 import { ref } from 'vue'
-import logoURL from '../assets/logo.png'
+import logoURL from '/../front-end/src/assets/logo.png'
 
 const is_expanded = ref(localStorage.getItem("is_expanded") === "true")
 
