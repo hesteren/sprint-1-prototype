a00e689ae9db6963cbdd7d81623df205bd850a65
Kester van Leeuwen <kester@posonas.com>
2022-09-22 17:45:39 +0200

    update router

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,12 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
       path: '/',
-      component: Home
+      name: 'home',
+      component: HomeView
     },
     {
       path: '/about',
